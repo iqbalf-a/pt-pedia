758e2d77658fb04da6ac2b58cfc710eb11f1e256
iqbal ardiansyah <dibul250899@gmail.com>
2024-05-12 21:52:06 +0700

    feat:add section: search, category

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CategoryComponent } from '../../section/category/category.component';
+import { ProductComponent } from '../../section/product/product.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CategoryComponent, ProductComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
